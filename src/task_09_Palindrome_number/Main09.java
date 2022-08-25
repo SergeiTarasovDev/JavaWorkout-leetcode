7459cfe2982a896518c5689e8cde21e6d942776d
Tarasov Sergei <www.neon@mail.ru>
2022-08-25 09:19:12 +0500

    Task 9. task solved

--- src/task_09_Palindrome_number/Main09.java
+++ src/task_09_Palindrome_number/Main09.java
@@ -2,6 +2,11 @@ package task_09_Palindrome_number;
 
 public class Main09 {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        solution.printSolution(121);
+        solution.printSolution(-121);
+        solution.printSolution(10);
+        solution.printSolution(95);
+        solution.printSolution(0);
     }
 }
