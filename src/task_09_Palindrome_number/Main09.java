637396798b4f5b727fe37a1638855070f3b866f1
Tarasov Sergei <www.neon@mail.ru>
2022-08-25 08:39:09 +0500

    Task 9. prepared for solution

---
+++ src/task_09_Palindrome_number/Main09.java
@@ -0,0 +1,7 @@
+package task_09_Palindrome_number;
+
+public class Main09 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/task_09_Palindrome_number/Solution.java
@@ -0,0 +1,7 @@
+package task_09_Palindrome_number;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+    }
+}
